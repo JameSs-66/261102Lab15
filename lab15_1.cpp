0567cd7091b2bfa855fc7e30346c19dccc89a995
JameSs- <thnwt.jsrt@gmail.com>
2023-01-17 16:44:31 +0700

    All Finished

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	int i, j;
+	T k;
+    for (i = 1; i < N; i++)
+    {
+        k = d[i];
+		for(j = i - 1; j >= 0 && d[j] < k;j--) d[j + 1] = d[j];
+        d[j + 1] = k;
+		cout << "Pass " << i << ":";
+		for(int x = 0; x < N; x++) cout << d[x] << " ";
+		cout << endl;
 
+    }
 }
 
 int main(){
@@ -17,3 +28,5 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
+
--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,36 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void findLocalMax(const double inputA[][N], bool outputB[][N]){
+		
+	for (int x = 0; x < N; x++) {
+        for (int y = 0; y < N; y++) outputB[x][y] = 0;
+	}
+	for (int i = 1; i < N-1; i++) {
+        for (int j = 1; j < N-1; j++) {
+                if(inputA[i][j] >= inputA[i-1][j] && inputA[i][j] >= inputA[i+1][j] && inputA[i][j] >= inputA[i][j-1] && inputA[i][j] >= inputA[i][j+1]) outputB[i][j] = 1;
+        }
+    }
+}
+
+void inputMatrix(double doubleA[][N]){
+	int i, j;
+	double inputA[N];
+	for (i = 0; i < N; i++){
+		cout << "Row " << i+1 << ": ";
+		for(int k = 0; k < N; k++) cin >> inputA[k];
+		for(j = 0; j < N; j++) doubleA[i][j] = inputA[j];
+	}
+}
+
+void showMatrix(const bool array[][N]){
+	int i,j;
+	for(i = 0; i < N; i++){
+		for(j = 0; j < N; j++){
+			cout << array[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+}
